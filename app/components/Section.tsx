6f0b9be602b5baadc6499fca3fb09d72f66b4a5f
mario-souvily <armco@hotmail.fr>
2025-08-08 11:47:54 +0200

    modification du status et ajout des images

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -2,8 +2,8 @@ import { cn } from "@/lib/utils";
 
 export const Section = ({ children, className }: { children: React.ReactNode, className?: string }) => {
   return (
-    <section className={cn("max-w-3xl px-4 m-auto", className)}>
-      <div className="flex flex-col gap-4 p-4">
+    <section className={cn(" m-auto", className)}>
+      <div className="flex flex-col gap-10 items-start">
         {children}
       </div>
     </section>
--- app/components/status.tsx
+++ app/components/status.tsx
@@ -5,38 +5,39 @@ import { Section } from "./Section";
 
 export const Status = () => {
   return (
-    <Section className="flex max-md:flex-col items-start gap-4">
-      <div className="flex-[3] w-full">
-        <Card className="w-full p-2 flex flex-col gap-2">
-          <p className="text-lg text-muted-foreground">Mes-projets</p>
-          <div className="flex flex-col gap-2">
-            {SideProjects.map((project, index) => (
-              <SideProject
-                key={index}
-                logo={project.logo}
-                title={project.title}
-                description={project.description}
-                url={project.url}
-              />
-            ))}
-          </div>
-        </Card>
-      </div>
-      <div className="flex-[2] w-full flex flex-col gap-4">
-        <Card className="p-2 flex-1">
-          Work
-        </Card>
-        <Card className="p-2 flex-1">
-          Contact
-        </Card>
-      </div>
+
+    <Section>
+
+      <Card className=" p-2 flex flex-col gap-2 ">
+        <p className="text-lg text-muted-foreground">Mes projets</p>
+        <div className="flex flex-col gap-2">
+          {SideProjects.map((project, index) => (
+            <SideProject
+              key={index}
+              title={project.title}
+              description={project.description}
+              url={project.url}
+              image={project.image}
+            />
+          ))}
+        </div>
+      </Card>
+      <Card className="p-2">
+        Mon travail
+      </Card>
+
+      <Card className="p-2">
+        Contact
+      </Card>
+
     </Section>
+
   );
 };
 
 const SideProjects = [
   {
-    image: "",
+    image: "/images/Zombieland-1.png",
     title: "Zombieland",
     description: "Site réalisé en automomie pour le passage de mon diplôme de développeur web fullstack qui a pour thème les zombies",
     url: "http://zombielandworld.surge.sh/"
@@ -56,23 +57,26 @@ const SideProjects = [
 ];
 
 type SideProjectProps = {
-  logo: string;
+
   title: string;
   description: string;
   url: string;
   image: string;
 };
 
+
+
 const SideProject = (props: SideProjectProps) => {
+
   return (
     <Link
       href={props.url}
       className="inline-flex items-center gap-4 hover:bg-accent hover:text-accent p-2 rounded-md"
     >
       <span className="bg-accent text-accent-foreground">
-        {props.image && (
-          <Image src={props.image} alt={props.title} width={16} height={16} />
-        )}
+
+        <Image src={props.image} alt={props.title} width={45} height={45} />
+
       </span>
       <div>
         <p className="text-lg font-semibold">{props.title}</p>
