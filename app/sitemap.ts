636940581b2c5d7e9647cebb7fe0448364dabcef
mario-souvily <armco@hotmail.fr>
2026-09-11 09:21:43 +0200

    add jardin and chemin pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,26 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.mariosouvily.com";
 
+  const lastModified = new Date().toISOString().split("T")[0]; // YYYY-MM-DD
+
   return [
     {
       url: baseUrl,
-      lastModified: new Date().toISOString().split("T")[0], // YYYY-MM-DD
+      lastModified,
       changeFrequency: "weekly",
       priority: 1.0,
     },
-    // Si un jour j'ajoute d'autres pages :
-    // {
-    //   url: `${baseUrl}/projets`,
-    //   lastModified: new Date().toISOString().split("T")[0],
-    //   changeFrequency: 'weekly',
-    //   priority: 0.8,
-    // },
+    {
+      url: `${baseUrl}/jardin/`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/chemin/`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
