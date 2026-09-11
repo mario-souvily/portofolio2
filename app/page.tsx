9708a8b7df0eb9c9476b774b057ae1adaa2b1803
mario-souvily <armco@hotmail.fr>
2026-09-11 12:24:10 +0200

    let project carousel bleed full width so side cards are not cut mid-screen

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,18 @@ export default function Home() {
   return (
     <main className="page-organique font-outfit relative min-h-screen overflow-hidden flex-1">
       <Blobs />
-      <div className="relative max-w-[1440px] mx-auto">
-        <NavBar />
-        <Hero />
+      <div className="relative">
+        <div className="max-w-[1440px] mx-auto">
+          <NavBar />
+          <Hero />
+        </div>
+        {/* Le carrousel prend toute la largeur : les cartes de côté
+            dépassent sur les bords de l'écran au lieu d'être coupées */}
         <ProjectCarousel />
-        <TeaserCards />
-        <ContactCta />
+        <div className="max-w-[1440px] mx-auto">
+          <TeaserCards />
+          <ContactCta />
+        </div>
       </div>
     </main>
   );
